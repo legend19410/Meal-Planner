27bf9458371bf19d3470556807d5a640b596e8b3
Jason Johnson <jvsonjohnson@gmail.com>
2021-03-31 21:47:05 -0500

    some frontend stuff

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from extension import mysql
 from routes.PopulateDBRoute import populate_db
 from routes.UpdateDBRoute import update_db
 from routes.QueryDBRoute import query_db
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 '''create flask app'''
 app = Flask(__name__)
@@ -23,7 +27,28 @@ app.register_blueprint(query_db, url_prefix="/query_db")
 
 @app.route('/')
 def index():
-    return "HOME"
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/sign-up')
+def signup():
+    return render_template("sign_up.html")
+
+@app.route('/recipe/<recipe_name>')
+def recipe(recipe_name):
+    return render_template("recipe.html", recipe_name=recipe_name)
+
+
+@app.route('/add-recipe')
+def add_recipe():
+    return render_template("input_recipe.html")
+
+@app.route('/create')
+def create():
+    return render_template("create_mplan.html")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
